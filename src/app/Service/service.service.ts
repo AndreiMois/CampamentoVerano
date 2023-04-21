df170d4b74f4953d00748c10550d76f487f79c5d
Andrei Mois <andreitarner103@gmail.com>
2023-04-21 12:43:26 +0200

    update

--- src/app/Service/service.service.ts
+++ src/app/Service/service.service.ts
@@ -23,6 +23,9 @@ export class ServiceService {
   getPersonaId(id:number){
     return this.http.get<Persona>(this.Url+"/"+id);
   }
+  updatePersona(persona:Persona){
+    return this.http.put<Persona>(this.Url+"/"+persona.id, persona);
+  }
 
   deletePersona(persona:Persona){
     return this.http.delete<Persona>(this.Url+"/"+persona.id);
